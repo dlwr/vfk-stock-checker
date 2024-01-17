6ff72df3732fd17d2a6b7122dbe05f77a0328b6f
dlwr <yuta25.a.k.a.djbudo.a.k.a.dlwr@gmail.com>
2024-01-17 18:50:34 +0900

    ヴァンフォーレ甲府ユニフォームのLとXLのサイズの在庫が復活したらWebhook URLに通知する

--- src/index.ts
+++ src/index.ts
@@ -12,39 +12,34 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { parse } from 'node-html-parser';
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-	//
-	// Example binding to a D1 Database. Learn more at https://developers.cloudflare.com/workers/platform/bindings/#d1-database-bindings
-	// DB: D1Database
+	DISCORD_WEBHOOK_URL: string;
 }
 
 export default {
 	// The scheduled handler is invoked at the interval set in our wrangler.toml's
 	// [[triggers]] configuration.
 	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
-		// A Cron Trigger can make requests to other endpoints on the Internet,
-		// publish to a Queue, query a D1 Database, and much more.
-		//
-		// We'll keep it simple and make an API call to a Cloudflare API:
-		let resp = await fetch('https://api.cloudflare.com/client/v4/ips');
-		let wasSuccessful = resp.ok ? 'success' : 'fail';
-
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+		const url = 'https://ventforet-shop.net/SHOP/11002.html';
+		const dom = await fetch(url)
+			.then((response) => response.text())
+			.then((text) => parse(text));
+		const lStatus = dom.querySelector('#itemStocklist > table > tbody > tr:nth-child(5) > td.stk > p')?.textContent || '×';
+		const xlStatus = dom.querySelector('#itemStocklist > table > tbody > tr:nth-child(6) > td.stk > p')?.textContent || '×';
+		if (lStatus !== '×' || xlStatus !== '×') {
+			await fetch(env.DISCORD_WEBHOOK_URL, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					content: `${url}
+在庫が復活！
+L:${lStatus}
+XL:${xlStatus}`,
+				}),
+			});
+		}
 	},
 };
